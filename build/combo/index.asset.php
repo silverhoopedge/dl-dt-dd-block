3d0466c3add5219feac02b447c2577737dc9d175
LisaS <lisasnyder@comcast.net>
2022-11-01 18:39:18 -0400

    initial commit

---
+++ build/combo/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '781a3550cd8e1ad1c2ff');
---
+++ build/dd-with-dt/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '56c472b73363421d5f0d');
